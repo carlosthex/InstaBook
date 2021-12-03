01a2db666e75e8501ebe9a2ead26694cc6eb05dc
CarlosThex <carlosthex@gmail.com>
2021-12-02 22:47:19 -0300

    som splash

--- app/src/main/java/com/example/instabook/ui/SplashInstaBook.java
+++ app/src/main/java/com/example/instabook/ui/SplashInstaBook.java
@@ -3,11 +3,13 @@ package com.example.instabook.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
 
 import com.example.instabook.R;
+
 
 public class SplashInstaBook extends AppCompatActivity {
 
@@ -16,11 +18,15 @@ public class SplashInstaBook extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_insta_book);
 
+        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.pagina);
+        mediaPlayer.start();
+
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent Login=new Intent(getApplicationContext(), TelaLogin.class);
+                mediaPlayer.stop();
                 startActivity(Login);
             }
         }, 2000);
